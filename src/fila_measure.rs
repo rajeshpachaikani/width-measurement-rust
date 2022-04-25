4f7fca2672270b452e5e144fae4c90b84a45c48f
Rajesh Pachaikani <rajeshpachaikani@live.in>
2022-04-25 16:06:43 +0530

    Filament Width Measurement displayed in console

--- src/fila_measure.rs
+++ src/fila_measure.rs
@@ -1,8 +1,8 @@
 use opencv::{
+    core,
+    imgproc,
     prelude::*,
     Result,
-    imgproc,
-    core,
 };
 use opencv::core::{Point, Scalar, Vec2b, Vec2f};
 
@@ -34,13 +34,13 @@ pub fn euclidean_distance(p1: &Point, p2: &Point) -> f64 {
 }
 
 pub fn get_normal(x: f64, y: f64, slope: f64, c: f64) -> (f64, f64) {
-    let slope_out = -1.0/slope;
+    let slope_out = -1.0 / slope;
     let intercept_out = y - slope_out * x;
     (slope_out, intercept_out)
 }
 
 //Get Line beginning point
-fn line_begins_at(line: &Vec2f) -> (f64, f64){
+fn line_begins_at(line: &Vec2f) -> (f64, f64) {
     let rho = line.0[0];
     let theta = line.0[1];
     let slope = get_slope(rho as f64, theta as f64);
@@ -48,32 +48,31 @@ fn line_begins_at(line: &Vec2f) -> (f64, f64){
 
     let point_1: (f64, f64) = get_point_with_y(slope, c, 0.0);
 
-    return point_1
-
+    return point_1;
 }
 
 // Split Lines
-pub fn split_lines(lines: &Mat) -> (Vec<i32>,Vec<i32>){
-    let mut left_indices : Vec<i32> = Vec::new();
-    let mut right_indices : Vec<i32> = Vec::new();
+pub fn split_lines(lines: &Mat) -> (Vec<u8>, Vec<u8>) {
+    let mut left_indices: Vec<u8> = Vec::new();
+    let mut right_indices: Vec<u8> = Vec::new();
 
-    if lines.empty(){
-        return (left_indices, right_indices)
+    if lines.empty() {
+        return (left_indices, right_indices);
     }
-    for i in 0..lines.rows(){
+    for i in 0..lines.rows() {
         let line = lines.at::<Vec2f>(i).unwrap();
         let point = line_begins_at(line);
-        if point.0 < 320.0{
-            left_indices.push(i);
-        }else{
-            right_indices.push(i);
+        if point.0 < 320.0 && point.0 > 20.0 {
+            left_indices.push(i as u8);
+        } else if point.0 > 320.0 && point.0 < 620.0 {
+            right_indices.push(i as u8);
         }
     }
-    return (left_indices, right_indices)
+    return (left_indices, right_indices);
 }
 
 //Draw Single Line
-pub fn draw_line(mut img: &mut Mat, line: &Vec2f){
+pub fn draw_line(mut img: &mut Mat, line: &Vec2f, color: &Scalar) {
     let rho = line.0[0];
     let theta = line.0[1];
     let slope = get_slope(rho as f64, theta as f64);
@@ -83,18 +82,18 @@ pub fn draw_line(mut img: &mut Mat, line: &Vec2f){
     let point_2: (f64, f64) = get_point_with_y(slope, c, 480.0);
 
     imgproc::line(&mut img,
-         Point::new(point_1.0 as i32, point_1.1 as i32),
-         Point::new(point_2.0 as i32, point_2.1 as i32),
-         Scalar::new(0., 0., 255., 0.),
-         2,
-         imgproc::LINE_AA,
-        0
+                  Point::new(point_1.0 as i32, point_1.1 as i32),
+                  Point::new(point_2.0 as i32, point_2.1 as i32),
+                  *color,
+                  2,
+                  imgproc::LINE_AA,
+                  0,
     );
 }
 
 
 // Draw Lines
-pub fn draw_lines( mut img : &mut Mat ,lines: &Mat) {
+pub fn draw_lines(mut img: &mut Mat, lines: &Mat) {
     if lines.rows() > 0 {
         for i in 0..lines.rows() {
             let rho_theta = lines.at::<Vec2f>(i).unwrap();
@@ -118,3 +117,73 @@ pub fn draw_lines( mut img : &mut Mat ,lines: &Mat) {
         }
     }
 }
+
+// Get Measurement from image
+pub fn get_measurement(mut img: &mut Mat) -> f64 {
+    let mut canny = Mat::default();
+    let mut image = img.clone();
+    let mut result: f64 = 0.0;
+    imgproc::canny(
+        &image,
+        &mut canny,
+        140.0,
+        200.0,
+        3,
+        false,
+    ).unwrap();
+
+    let mut lines = Mat::default();
+    imgproc::hough_lines(
+        &canny,
+        &mut lines,
+        1.0,
+        std::f64::consts::PI / 180.0,
+        100,
+        0.0,
+        0.0,
+        0.0,
+        std::f64::consts::PI,
+    ).unwrap();
+
+    // draw_lines(&mut img, &lines);
+    if !lines.empty() {
+        let left_right = split_lines(&lines);
+        //println!("Left Index{:?}", left_right.0);
+        // println!("Right Index{:?}", left_right.1);
+        if !left_right.0.is_empty() && !left_right.1.is_empty() {
+            let line_left = lines.at::<Vec2f>(left_right.0[0] as i32).unwrap();
+
+            let line_right = lines.at::<Vec2f>(left_right.1[0] as i32).unwrap();
+            let left_color = Scalar::new(0.0, 255.0, 0.0, 255.0);
+            let right_color = Scalar::new(0.0, 0.0, 255.0, 255.0);
+            draw_line(&mut img, &line_left, &left_color);
+            draw_line(&mut img, &line_right, &right_color);
+
+            //Slope and intercept of left line
+            let slope_left = get_slope(line_left.0[0] as f64, line_left.0[1] as f64);
+            let intercept_left = get_y_intercept(line_left.0[0] as f64, line_left.0[1] as f64);
+
+            //Slope and intercept of right line
+            let slope_right = get_slope(line_right.0[0] as f64, line_right.0[1] as f64);
+            let intercept_right = get_y_intercept(line_right.0[0] as f64, line_right.0[1] as f64);
+
+            // Get point at y = 240
+            let point_left = get_point_with_y(slope_left, intercept_left, 240.0);
+
+            // Get Perpendicular line
+            let normal_line = get_normal(point_left.0, point_left.1, slope_left, intercept_left);
+
+            // Get Intersection point
+            let intersection_point = get_intersection_point(normal_line.0, normal_line.1, slope_right, intercept_right);
+
+            let point_l = Point::new(point_left.0 as i32, point_left.1 as i32);
+            let point_r = Point::new(intersection_point.0 as i32, intersection_point.1 as i32);
+
+            let dist = euclidean_distance(&point_l, &point_r);
+            // println!("Filament Width in mm: {:?}", dist * 0.009375);
+            result = dist * 0.009375;
+        }
+    }
+    result
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
     Author: Rajesh Pachaikani
     Github: github.com/rajeshpachaikani
     Title: Filament Measurement
-    Description: To measure and display the filament width using a digital microsope.
+    Description: To measure and display the filament width using a digital microscope.
     Calibration values:
         The camera is focused to capture 6mm x 4.5 mm
         The camera resolution is 640x480 in pixels
@@ -12,110 +12,27 @@
 
 use opencv::{
     highgui,
-    videoio,
     imgproc,
-    Result,
     prelude::*,
+    Result,
+    videoio,
+    core::{Point, Point_, Scalar, Vec2b, Vec2f},
 };
-use opencv::core::{Point, Point_, Scalar, Vec2b, Vec2f};
 
 mod fila_measure;
 
 fn main() -> Result<()> {
     let mut cam = videoio::VideoCapture::new(2, videoio::CAP_ANY)?;
+
     let mut img = Mat::default();
-    let mut canny = Mat::default();
     loop {
         cam.read(&mut img)?;
-        imgproc::canny(
-            &img,
-            &mut canny,
-            100.0,
-            200.0,
-            3,
-            false,
-        )?;
-
-        // Find Hough Lines
-        let mut lines = Mat::default();
-        imgproc::hough_lines(
-            &canny,
-            &mut lines,
-            1.0,
-            std::f64::consts::PI / 180.0,
-            100,
-            0.0,
-            0.0,
-            0.0,
-            std::f64::consts::PI,
-        )?;
-
-        // fila_measure::draw_lines(&mut img, &lines);
-        if !lines.empty() {
-            let left_right = fila_measure::split_lines(&lines);
-            //println!("Left Index{:?}", left_right.0);
-            // println!("Right Index{:?}", left_right.1);
-            if !left_right.0.is_empty() && !left_right.1.is_empty() {
-                let line_left = lines.at::<Vec2f>(left_right.0[0])?;
-                let line_right = lines.at::<Vec2f>(left_right.1[0])?;
-                fila_measure::draw_line(&mut img, &line_left);
-                fila_measure::draw_line(&mut img, &line_right);
-
-                //Slope and intercept of left line
-                let slope_left = fila_measure::get_slope(line_left.0[0] as f64, line_left.0[1] as f64);
-                let intercept_left = fila_measure::get_y_intercept(line_left.0[0] as f64, line_left.0[1] as f64);
-
-                //Slope and intercept of right line
-                let slope_right = fila_measure::get_slope(line_right.0[0] as f64, line_right.0[1] as f64);
-                let intercept_right = fila_measure::get_y_intercept(line_right.0[0] as f64, line_right.0[1] as f64);
-
-                // Get point at y = 240
-                let point_left = fila_measure::get_point_with_y(slope_left, intercept_left, 240.0);
-
-                // Get Perpendicular line
-                let normal_line = fila_measure::get_normal(point_left.0, point_left.1, slope_left, intercept_left);
-
-                // Get Intersection point
-                let intersection_point = fila_measure::get_intersection_point(normal_line.0, normal_line.1, slope_right, intercept_right);
-
-                let point_l = Point::new(point_left.0 as i32, point_left.1 as i32);
-                let point_r = Point::new(intersection_point.0 as i32, intersection_point.1 as i32);
-
-                // println!("{:?}", intersection_point);
-                imgproc::line(
-                    &mut img,
-                    Point::new(point_left.0 as i32, point_left.1 as i32),
-                    Point::new(intersection_point.0 as i32, intersection_point.1 as i32),
-                    Scalar::new(0.0, 255.0, 0.0, 0.0),
-                    1,
-                    imgproc::LINE_AA,
-                    0,
-                )?;
-                imgproc::circle(
-                    &mut img,
-                    Point::new(intersection_point.0 as i32, intersection_point.1 as i32),
-                    5,
-                    Scalar::new(255., 0., 255., 0.),
-                    5,
-                    imgproc::LINE_AA,
-                    0,
-                )?;
-                let dist = fila_measure::euclidean_distance(&point_l, &point_r);
-                println!("Filament Width in mm: {:?}", dist*0.009375);
-                // println!("{:?}", fila_measure::euclidean_distance(&point_l, &point_r));
-            }
-
-            // println!("{:?}", dist);
-
-        }
-        highgui::imshow("Canny", &canny)?;
+        print!("Filament width in mm::{}\n", fila_measure::get_measurement(&mut img));
         highgui::imshow("Filament Measurement", &img)?;
-        let key = highgui::wait_key(1)?;
-        if key == 27 || key == 'q' as i32 {
+        if highgui::wait_key(1)? == 27 {
             break;
         }
     }
-
 
     Ok(())
 }
